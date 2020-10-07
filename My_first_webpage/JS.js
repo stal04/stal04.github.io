d0566da91d7eade7ed34a9b5cbabb2a1b7026842
stal04 <36510570+stal04@users.noreply.github.com>
2020-10-07 20:41:30 +0200

    Neviem dalej

---
+++ My_first_webpage/JS.js
@@ -0,0 +1,36 @@
+const photo       = $("#photo"),
+      title       = $("#photo-title"),
+      description = $("#photo-description"),
+      back        = $("#back"),
+      forward     = $("#forward");
+/*
+let image1 = {
+    photo: "images/krivan.jpg",
+    title: "Kriváň",
+    description: "My description",
+};
+
+let image2 = {
+    photo: "images/gerlach.jpg",
+    title: "Gerlach",
+    description: "My description",
+};
+
+let images_data =[image1,image2]
+let currentPhoto = 0; */
+
+let data = {
+    photo: 'images/krivan.jpg',
+    title: 'My title',
+    description: 'What happened here, why is this a very nice image'
+  };
+
+$('#photo').attr('src', data.photo);
+
+let loadPhoto = (photoNumber) => {
+    $('#photo').attr('src', imagesData[photoNumber].photo);
+    // ...
+  }
+  
+  loadPhoto(currentPhoto);
+ 
