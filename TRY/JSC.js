8ec161f53f6b395fc896c833ce1143b4d41bbe88
stal04 <36510570+stal04@users.noreply.github.com>
2020-10-09 14:40:10 +0200

    zmeny klik

--- TRY/JSC.js
+++ TRY/JSC.js
@@ -1,30 +1,35 @@
 /*$("h1").text("hell");*/
 
-const photo       = $("#photo"),
-      title       = $("#photo-title"),
-      description = $("#photo-description"),
-      back        = $("#back"),
-      forward     = $("#forward");
+const photo = $("#photo"),
+    title = $("#photo-title"),
+    description = $("#photo-description"),
+    back = $("#back"),
+    forward = $("#forward");
 
 let data = [{
- photos : "/stal04.github.io/My_first_webpage/images/pohoda2.jpg",
- titles : "Pohoda",
- descriptions :"Biggest slovakian festival, always great time!",
-},{
-photos : "/stal04.github.io/My_first_webpage/images/trnava.jpg",
-titles : "Trnava",
-descriptions :"Vlacik",
+    photos: "/stal04.github.io/My_first_webpage/images/VT.jpg",
+    titles: "Vysoké Tatry",
+    descriptions: "Biggest slovakian festival, always great time!",
+}, {
+    photos: "/stal04.github.io/My_first_webpage/images/trnava.jpg",
+    titles: "Trnava",
+    descriptions: "Vlacik",
 },
 {
-photos : "/stal04.github.io/My_first_webpage/images/bratislava.jpg",
-titles : "Bratislava",
-descriptions :"Nieco sa na nom stalo",
+    photos: "/stal04.github.io/My_first_webpage/images/bratislava.jpg",
+    titles: "Bratislava",
+    descriptions: "Nieco sa na nom stalo",
 },
 {
-photos : "/stal04.github.io/My_first_webpage/images/bojnice.jpg",
-titles : "Bojnice castle",
-descriptions :"Nieco sa na nom stalo",
-        },
+    photos: "/stal04.github.io/My_first_webpage/images/bojnice.jpg",
+    titles: "Bojnice castle",
+    descriptions: "Nieco sa na nom stalo",
+},
+{
+    photos: "/stal04.github.io/My_first_webpage/images/pohoda2.jpg",
+    titles: "Pohoda",
+    descriptions: "Nieco sa na nom stalo",
+},
 ]
 
 let currentPhoto = 0;
@@ -48,7 +53,7 @@ data.forEach((picture) => {
     <img class="thumbnail-img" src="${data[dataNum].photos}" data-number="${dataNum}" alt="">
   </div>`;;
     $(".thumbnail").append(thumbnail);
-  });
+});
 
 
 
@@ -57,6 +62,15 @@ $("#photo-title").text(data[currentPhoto].titles);
 $("#photo-description").text(data[currentPhoto].descriptions);
 
 
-$(".thumbnail").on("click", (event) => {
-    console.log("Hello thumbnail")
-  });
+let loadPhoto = (photoNumber) => {
+    $("#photo").attr("src", data[currentPhoto].photos);
+    $("#photo-title").text(data[currentPhoto].titles);
+    $("#photo-description").text(data[currentPhoto].descriptions);
+};
+
+$(".thumbnail-img").on("click", (event) => {
+    currentPhoto = $(event.target).data("number");
+    console.log(currentPhoto);
+    loadPhoto(currentPhoto);
+});
+
